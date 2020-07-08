66af87122a8d8f5f3f7b59ba7343cb180bd2b2bf
tfkproject <taufik.y2t@gmail.com>
2020-07-08 22:42:37 +0700

    Fix bold

--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -5,7 +5,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini">App</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b><?php echo SITE_NAME ?></span>
+      <span class="logo-lg"><b><?php echo SITE_NAME ?></b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
