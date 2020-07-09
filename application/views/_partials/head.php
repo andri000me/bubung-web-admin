b6d0cbf029f0890dc94bec30b41f4dc36c38053e
tfkproject <taufik.y2t@gmail.com>
2020-07-09 22:57:47 +0700

    Update (change) position script

--- application/views/_partials/head.php
+++ application/views/_partials/head.php
@@ -40,4 +40,7 @@
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  <!-- jQuery 3 -->
+  <script src="<?php echo base_url('bower_components/jquery/dist/jquery.min.js') ?>"></script>
 </head>
--- application/views/_partials/script.php
+++ application/views/_partials/script.php
@@ -1,6 +1,3 @@
-
-<!-- jQuery 3 -->
-<script src="<?php echo base_url('bower_components/jquery/dist/jquery.min.js') ?>"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="<?php echo base_url('bower_components/jquery-ui/jquery-ui.min.js') ?>"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
